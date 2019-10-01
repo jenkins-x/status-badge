6799945f488a293cff2baafa3d21b00639b79cfb
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-10-01 20:43:50 +0100

    fix: corrected downstream build with app name

--- updatebot.sh
+++ updatebot.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-ORG=jenkinsxio
 APP_NAME=status-badge
 
-jx step create pr chart --name $ORG/$APP_NAME --version ${VERSION} --repo https://github.com/jenkins-x-apps/jx-app-statusbadge.git
+jx step create pr chart --name $APP_NAME --version ${VERSION} --repo https://github.com/jenkins-x-apps/jx-app-statusbadge.git
